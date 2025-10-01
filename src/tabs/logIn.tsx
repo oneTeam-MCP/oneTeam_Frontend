48492622c98ed912a8aff228fb99dc3ea6049d72
Maeng Uihyeon <moh45@naver.com>
2025-10-01 14:19:45 +0900

    ft. 오류 수정

--- src/tabs/logIn.tsx
+++ src/tabs/logIn.tsx
@@ -39,7 +39,7 @@ export default function Login() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           userId: e.StudentNum,
-          password: e.Password,
+          userPw: e.Password,
           action: "login",
         }),
       }
